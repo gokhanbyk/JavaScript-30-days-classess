9ff00eea56428d876372d3f710a9d23a2fb18d98
Gökhan Bıyıkoğlu <105162003+gokhanbyk@users.noreply.github.com>
2023-07-12 17:09:43 +0300

    Uploading Day 16 Class

--- Day 16/script.js
+++ Day 16/script.js
@@ -1,2 +1,269 @@
 // DAY 16 JSON
 
+// JSON: JSON stands for JavaScript Object Notation. The JSON syntax is derived from JavaScript object notation syntax, but the JSON format is text or string only. JSON is a light weight data format for storing and transporting. JSON is mostly used when data is sent from a server to a client. JSON is an easier-to-use alternative to XML.
+
+let users1 = {
+    "users":[
+        {
+        "firstName":"Asabeneh",
+        "lastName":"Yetayeh",
+        "age":250,
+        "email":"asab@asb.com"
+        },
+        {
+        "firstName":"Alex",
+        "lastName":"James",
+        "age":25,
+        "email":"alex@alex.com"
+        },
+        {
+        "firstName":"Lidiya",
+        "lastName":"Tekle",
+        "age":28,
+        "email":"lidiya@lidiya.com"
+        }
+    ]
+}
+// JSON object should be with double quotes or it should be a string.
+
+// we use "firstNaMe" instead of just firstName, however in object we use keys without double quotes. This is the major difference between an object and a JSON.
+let user = {
+    "Alex": {
+        "email": "alex@alex.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 30
+    },
+    "Asab": {
+        "email": "asab@asab.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "Redux",
+            "MongoDB",
+            "Express",
+            "React",
+            "Node"
+        ],
+        "age": 25,
+        "isLoggedIn": false,
+        "points": 50
+    },
+    "Brook": {
+        "email": "daniel@daniel.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "React",
+            "Redux"
+        ],
+        "age": 30,
+        "isLoggedIn": true,
+        "points": 50
+    },
+    "Daniel": {
+        "email": "daniel@alex.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "Python"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 40
+    },
+    "John": {
+        "email": "john@john.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "React",
+            "Redux",
+            "Node.js"
+        ],
+        "age": 20,
+        "isLoggedIn": true,
+        "points": 50
+    },
+    "Thomas": {
+        "email": "thomas@thomas.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "React"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 40
+    },
+    "Paul": {
+        "email": "paul@paul.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "MongoDB",
+            "Express",
+            "React",
+            "Node"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 40
+    }
+}
+
+
+// Converting JSON to JavaScript Object
+
+// JSON.parse()
+
+// JSON.parse(json[, reviver])
+// // json or text , the data
+// // reviver is an optional callback function
+// /* JSON.parse(json, (key, value) => {
+
+// })
+// */
+
+const usersText = `{
+    "users":[
+      {
+        "firstName":"Gökhan",
+        "lastName":"Bıyıkoğlu",
+        "age":21,
+        "email":"gokhanbyk@asdas.com"
+      },
+      {
+        "firstName":"Alex",
+        "lastName":"James",
+        "age":25,
+        "email":"alex@alex.com"
+      },
+      {
+      "firstName":"Lidiya",
+      "lastName":"Tekle",
+      "age":28,
+      "email":"lidiya@lidiya.com"
+      }
+    ]
+}`
+    
+let usersObj = JSON.parse(usersText, undefined, 4)
+console.log(usersObj)
+
+// Using a reviver function with JSON.parse()
+usersObj = JSON.parse(usersText, (key, value) => {
+    let newValue =
+        typeof value == 'string' && key != 'email' ? value.toUpperCase() : value
+    return newValue
+})
+console.log(usersObj)
+
+// Converting Object to JSON
+// JSON.stringify()
+
+/*
+JSON.stringify(obj, replacer, space)
+// json or text , the data
+// reviver is an optional callback function
+*/
+
+const users = {
+    Alex: {
+      email: 'alex@alex.com',
+      skills: ['HTML', 'CSS', 'JavaScript'],
+      age: 20,
+      isLoggedIn: false,
+      points: 30
+    },
+    Asab: {
+      email: 'asab@asab.com',
+      skills: [
+        'HTML',
+        'CSS',
+        'JavaScript',
+        'Redux',
+        'MongoDB',
+        'Express',
+        'React',
+        'Node'
+      ],
+      age: 25,
+      isLoggedIn: false,
+      points: 50
+    },
+    Brook: {
+      email: 'daniel@daniel.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+      age: 30,
+      isLoggedIn: true,
+      points: 50
+    },
+    Daniel: {
+      email: 'daniel@alex.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+      age: 20,
+      isLoggedIn: false,
+      points: 40
+    },
+    John: {
+      email: 'john@john.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+      age: 20,
+      isLoggedIn: true,
+      points: 50
+    },
+    Thomas: {
+      email: 'thomas@thomas.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+      age: 20,
+      isLoggedIn: false,
+      points: 40
+    },
+    Paul: {
+      email: 'paul@paul.com',
+      skills: [
+        'HTML',
+        'CSS',
+        'JavaScript',
+        'MongoDB',
+        'Express',
+        'React',
+        'Node'
+      ],
+      age: 20,
+      isLoggedIn: false,
+      points: 40
+    }
+}
+  
+const txt = JSON.stringify(users, undefined, 4)
+console.log(txt)
+
+
+// Using a Filter Array with JSON.stringify
+const user1 = {
+    firstName: 'Asabeneh',
+    lastName: 'Yetayeh',
+    country: 'Finland',
+    city: 'Helsinki',
+    email: 'alex@alex.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Python'],
+    age: 250,
+    isLoggedIn: false,
+    points: 30
+  }
+  
+const txt1 = JSON.stringify(user1,['firstName', 'lastName', 'country', 'city', 'age'],4)
+console.log(txt1)
